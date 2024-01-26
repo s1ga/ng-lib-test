ee5c1d243864f1edbd78d943d8603b21be041cb4
s1ga <s.v.harlanov@gmail.com>
2024-01-26 21:45:46 +0100

    feat: generate project

---
+++ projects/showcase/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LibComponent } from '../../../lib/src/lib/lib.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, LibComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Lib showcase';
+}
